74b05c5bab693060f00612d722781b97f37cddc7
hongyuanhua <hhy981123@gmail.com>
2020-11-30 23:32:52 -0500

    update

--- devflow_backend/server.js
+++ devflow_backend/server.js
@@ -1,13 +1,12 @@
 // Set up router
 const express = require("express");
 const app = express();
-var cors = require('cors')
-app.use(cors())
+var cors = require("cors");
+app.use(cors());
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 const session = require("express-session");
-
 
 const auth = require("./routes/auth");
 const member = require("./routes/member");
@@ -20,20 +19,21 @@ const { Team } = require("./models/Team");
 
 // Set up database
 const mongoose = require("mongoose");
-const MONGODB = "mongodb+srv://team34:team34Password@devflow.oddf0.mongodb.net/devflow";
+const MONGODB =
+  "mongodb+srv://team34:team34Password@devflow.oddf0.mongodb.net/devflow";
 mongoose.set("useNewUrlParser", true);
 mongoose.set("useUnifiedTopology", true);
 mongoose.set("useCreateIndex", true);
 mongoose.set("useFindAndModify", false);
 mongoose
-    .connect(MONGODB)
-    .then(() => console.log("Connected to MongoDB..."))
-    .catch((err) => console.log("Could not connect to MongoDB", err));
+  .connect(MONGODB)
+  .then(() => console.log("Connected to MongoDB..."))
+  .catch((err) => console.log("Could not connect to MongoDB", err));
 
 const port = process.env.PORT || 5000;
 console.log("starting the server...");
 app.listen(port, () => {
-    console.log(`Listening on port ${port}...`);
+  console.log(`Listening on port ${port}...`);
 });
 
 const { nanoid } = require("nanoid");
@@ -41,24 +41,25 @@ console.log(nanoid());
 
 // start routing
 const sessionChecker = (req, res, next) => {
-    if (!req.session.memberId) {
-        res.status(401).send("Not logged in");
-    } else {
-        next();
-    }
+  console.log(req.session);
+  if (!req.session.memberId) {
+    res.status(401).send("Not logged in");
+  } else {
+    next();
+  }
 };
 
 app.use(
-    session({
-        secret: "our hardcoded secret",
-        resave: true,
-        saveUninitialized: true,
-        cookie: {
-            expires: 600000,
-            httpOnly: true,
-            secure: false
-        }
-    })
+  session({
+    secret: "our hardcoded secret",
+    resave: true,
+    saveUninitialized: true,
+    cookie: {
+      expires: 600000,
+      httpOnly: true,
+      secure: false,
+    },
+  })
 );
 
 // // Express middleware
@@ -69,6 +70,3 @@ app.use("/api", sessionChecker);
 // Routing
 app.use("/auth", auth);
 app.use("/api/member", member);
-
-
-
