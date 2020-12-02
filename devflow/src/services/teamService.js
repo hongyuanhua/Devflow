1929d5f9684a76b19ceda40d7e38ed00c81a0a72
troyyxk <xingkunyin@hotmail.com>
2020-12-02 00:45:04 -0500

    Add team

--- devflow/src/services/teamService.js
+++ devflow/src/services/teamService.js
@@ -1,3 +1,13 @@
 import { config } from "../config.js";
 const { backend } = config;
 const { host, port } = backend;
+
+// module.export = { login: login };a
+export const getTeamById = async (id) => {
+    console.log("---getMemberByToId---");
+    // Create our request constructor with all the parameters we need
+    const url = host + port + "/api/team/" + id;
+    // Send the request with fetch()
+    const team = await fetch(url);
+    return team;
+};
--- devflow_backend/routes/team.js
+++ devflow_backend/routes/team.js
@@ -2,6 +2,20 @@ const express = require("express");
 const router = express.Router();
 const { Team } = require("../models/Team.js");
 
+router.get("/:id", async (req, res) => {
+  console.log("---Get team by id---")
+  const teamId = req.params.id;
+  Team.findById(teamId)
+    .then(team => {
+      if (!team) {
+        return res.status(404).send("No such team");
+      }
+      console.log("success get team by id")
+      res.send(team);
+    })
+    .catch(err => res.status(500).send("Server err"))
+});
+
 router.get("/all", async (req, res) => {
   Team.find()
     .then((teams) => {
