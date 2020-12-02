5d601ea53442c271a79828eeb01bce7b9b40fac6
Ensouled <58400501+Ensouled@users.noreply.github.com>
2020-12-01 22:59:36 -0500

    finished notification

--- devflow/src/services/memberService.js
+++ devflow/src/services/memberService.js
@@ -1,28 +1,13 @@
-import axios from "axios";
 import { config } from "../config.js";
 const { backend } = config;
 const { host, port } = backend;
 
-// getMembers
-export const getMembers = (userName, password) => {
-  console.log(userName, password);
-  const addr = host + port + "/auth/login";
-  axios
-    .post(addr, {
-      // request body
-      data: {
-        userName: userName,
-        password: password,
-      },
-      headers: {
-        "content-type": "application/json",
-      },
-      withCredentials: true,
-    })
-    .then((res) => {
-      console.log(res);
-    })
-    .catch((err) => console.log(err));
-};
-
 // module.export = { login: login };a
+export const getMemberById = async (id) => {
+  console.log("---getMemberByToId---");
+  // Create our request constructor with all the parameters we need
+  const url = host + port + "/api/member/" + id;
+  // Send the request with fetch()
+  const member = await fetch(url);
+  return member;
+};
