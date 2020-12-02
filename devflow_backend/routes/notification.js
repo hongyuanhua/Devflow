4134ee6f9967f98b49df3c49899c33f4e05ec0d6
Ensouled <58400501+Ensouled@users.noreply.github.com>
2020-12-01 21:55:31 -0500

    Add notification backend

--- devflow_backend/routes/notification.js
+++ devflow_backend/routes/notification.js
@@ -14,5 +14,16 @@ router.get("/to/:id", async (req, res) => {
     })
     .catch((err) => res.status(500).send("Server err"));
 });
-
+router.get("/from/:id", async (req, res) => {
+  console.log("---GetNotificationByFromId---");
+  const memberId = req.params.id;
+  Notification.find({ from: memberId })
+    .then((notifications) => {
+      if (!notifications) {
+        return res.status(404).send("No such notifications");
+      }
+      res.send(notifications);
+    })
+    .catch((err) => res.status(500).send("Server err"));
+});
 module.exports = router;
